ed176a0a9c01c709a6158d496483b7e154833394
CloverCas <99794007+CloverCas@users.noreply.github.com>
2022-05-21 21:14:12 +0800

    Delete LT5 directory

--- LT5/script.js
+++
@@ -1,5 +0,0 @@
-var urlParams = new URLSearchParams(window.location.search);
-console.log(urlParams);
-var ref =document.getElementById('myName');
-ref.innerText=urlParams.get('myName');
-console.log(ref);
