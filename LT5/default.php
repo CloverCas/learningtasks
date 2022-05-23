7685da7a44492279d84565542c98cf9ef3facbec
CloverCas <99794007+CloverCas@users.noreply.github.com>
2022-05-23 12:18:58 +0800

    Add files via upload

---
+++ LT5/default.php
@@ -0,0 +1,11 @@
+<?php
+$page = (isset($_GET['page']) && $_GET['page'] != '') ? $_GET['page'] : '';
+?>
+<!DOCTYPE html>
+<html>
+<title> CloverTv </title>
+<link rel="stylesheet" type="text/css" href="stylesheet.css"/>
+<center>
+<h1>PROFILE</h1></center>
+</center>
+</html>
